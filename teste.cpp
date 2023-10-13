c8b0fe29ad4a47c6e13cb4ab64d3a829614561bc
mvvalerio <martim.enta@hotmail.com>
2023-10-13 14:16:38 +0000

    commit

---
+++ teste.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+using namespace std;
+
+int O_MEU_NUMERO = 10;                       // Números inteiros
+double O_MEU_NUMERO_REAL = 10.5;             // Números reais
+char O_MEU_CARACTER = 'a';                   // Caracteres
+string O_MEU_TEXTO = "Isto é um texto";      // Texto
+
+bool O_MEU_BOOLEANO = true;                  // Booleano
+// int mybool = 1                            // True
+// int mybool = 0                            // False
+
+// Tipo de variavel  -  Nome da variavel  =  Valor da variavel
+
+int sensorTemperatura = 10;                  // Variavel Inteira (Cammel Case) "Maiuscula -> Minuscula"
+const int A_MINHA_CONTANTE = 1234;           // Variavel Inteira
+const double PI = 3.1415;                    // Constante
+int x = 2, y = 2, z = 14;
+int minutosPorHora = 60;
+
+int numero;                                  // Não tem valor "Null"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int NUMPORT = 5;                            // Porta de entrada do sensor de humidade
+
+/*
+*
+* @author é iê
+* teste
+*
+*/
+
+/**
+*
+* @brief 
+*
+* @param a numero inteiro
+*
+*/
+
+
+int NUM_PORT_SENS_HUM = 5;
+
+ /**
+   int main()
+{
+    cout << "Hello Mundo!\t";              // Isto é um padeiro a falar
+    cout << "I'm a C++ programa\n";
+    cout << O_MEU_NUMERO << "\n";
+    cout << O_MEU_CARACTER << "\n";
+
+    return 0;
+}
+*/
+
+ /**
+int main()
+{
+    cout << PI << " Valor de PI\n";
+    cout << sensorTemperatura << " Valor do Sensor de Temperatura\n";
+
+    sensorTemperatura = 20;
+
+    cout << sensorTemperatura << " Valor do Sensor de Temperatura\n";
+
+    return 0;
+}
+*/
+
+/**int main()
+{
+    cout << "Digita um numero: ";           // Imprime no ecrã
+    cin >> numero;                          // Lê o teclado
+    cout << "O numero que digitaste foi:" << numero << endl;
+    return 0;              
+}
+*/
+
+/** int main()
+{
+    int a = 1;
+    int b = 2;
+    cout << a+b << endl;
+    return 0;
+}
+*/
+//tendo em consideração que a área do circulo
+//é PI * raio * raio, calcular a área do circulo
+//com raio 10
+
+/**int main()
+{
+int raio;
+cout << "Qual a área do circulo?" << endl;
+cin >> raio;
+double area = PI * raio * raio;
+cout << area << endl;
+return 0;
+}
+*/
+
+int main()                                 
+{
+    /* int a = 4;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int conta = a + (b * c) + d;            // Conta de soma e multiplicação "4+(5x2)+1".
+    */
+    int a = 5;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+   // int conta = a%c;                        "%" é o resto da sobra da divisão da conta "5/2 = 2.5" ".5 + .5 = 1". 
+   // int conta = ++a;                        "++" adiciona "1"
+   // int conta = --a;                        "--" subtrai "1"
+   /* int conta = a+=2;                       "+=" atribui um valor
+    conta = a+=1;
+    */
+    int conta = a&=6;                       //"&" se for digitado o valor de "a" dá o valor de "a", caso for diferente de "a" dará 0.
+    cout << conta << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+/* !!!! Notas !!!!
+ * 
+ * 1 Byte tem 8 Bits
+ * 
+ * string é um conjunto char
+ * 
+ * Nome de uma variavel deve ser escrita em "Cammel Case"
+ * 
+ * "const" abreviatura de constante, ou seja, não pode ser alterado
+ * 
+ * "cout" - output
+ * 
+ * "cin" - input
+ * 
+ * coocatenação é para juntar uma ou mais coisas
+ * 
+ * 
+ * 
+*/
