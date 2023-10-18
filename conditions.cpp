9f7dc9b75c90ce1bfa5af50560d373c53b97d2f3
mvvalerio <martim.enta@hotmail.com>
2023-10-18 12:37:04 +0000

    conditions.cpp

---
+++ conditions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int a=10, b=12, c=9;
+
+int main(){
+
+if ( a < b ){                            // "||" esse ou o outro, "&&" e o outro
+    cout << "é vidade" << endl;
+} else if ( a < c ){
+    cout << "é vidade" << endl;
+    }else{
+    cout << "é mintixra" << endl;
+    }
+return 0;
+}
