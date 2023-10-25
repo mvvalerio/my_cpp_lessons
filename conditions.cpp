3ff2f1be1c3348e8ea63f7170e9dbe0a6a2fd39b
mvvalerio <martim.enta@hotmail.com>
2023-10-25 11:33:20 +0000

    conditions.cpp

--- conditions.cpp
+++ conditions.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int a=10, b=12, c=9;
+/**int a=10, b=12, c=9;
 
 int main(){
 
@@ -14,3 +14,25 @@ if ( a < b ){                            // "||" esse ou o outro, "&&" e o outro
     }
 return 0;
 }
+*/
+
+/**
+ * @brief peça um número e mostre se ele é positivo ou negativo
+ * 
+ * @return int
+ * 
+*/
+
+int main(){
+    int num;
+
+    cout << "Atira prai um número";
+    cin >> num;
+
+    if (num >= 0){
+        cout << "É Positivo naião" << endl;
+    } else {
+     cout << "É Negativo Padeiro" << endl;
+    }
+    return 0;
+    } 
