6618d3c9ea69015f72839f221791d057caef1b57
mvvalerio <martim.enta@hotmail.com>
2023-10-27 10:28:57 +0000

    loopFor.cpp

---
+++ loopFor.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+//int chave[5];
+//int estrelas[2];
+//for (int i = 0; i < 4; i++){
+// cout << chave[i] << endl;
+//}
+
+string cars[] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+cout << cars[2] << endl;
+cars [2] = "Opel";
+cout << cars[2] << endl;
+
+
+
+
+
+//while (i != 10){
+//    cout << "Bom dia" << endl;
+//}
+
+
+//do {
+//    cout << "Boa Tarde" << endl;
+//} while ( i != 10);
+
+//cout << "inicio do ciclo For" << endl;
+//  for (int n=0; n<=10; n++){
+//       if ( n == 4 ){
+//        continue;
+//     }
+//        cout << n << endl;
+//  }
+//cout << "Fim do ciclo For" << endl;
+//
+   return 0;
+}
