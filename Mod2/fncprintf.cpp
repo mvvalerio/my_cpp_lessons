002f5b6ac85223dfee4c1a28149941f5340f9d89
Martim Valério <martimvv07@gmail.com>
2024-02-06 10:35:38 -0100

    Updated

--- Mod2/fncprintf.cpp
+++ Mod2/fncprintf.cpp
@@ -1,23 +1,28 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 
 using namespace std;
 
+    char nome [50];
+    string str;
+    int i;
 
 int main(){
 
-char nome [50];
+/*        printf("Teste\n");
+        printf("Character: %s %s %s \n", "ey", "ty", "teste");
+        printf("Decimals: %d %ld \n", 1977, 650000L);
+        printf("Preceding with blanks: %10d \n", 1977);
+        printf("Width trick: %*d \n", 10, 10);
 
-    printf("Teste\n");
-    printf("Character: %s %s %s \n", "ey", "ty", "teste");
-    printf("Decimals: %d %ld \n", 1977, 650000L);
-    printf("Preceding with blanks: %10d \n", 1977);
-    printf("Width trick: %*d \n", 10, 10);
+        printf("Nome \n");
+        scanf("%s", nome);                                      //scanf = vai dar input
+        printf("Ola %s \n", nome);
+*/
 
-    printf("Nome \n");
-    scanf("%s", nome);                                      //scanf = vai dar input
-    printf("Ola %s \n", nome);
+        printf("Enter your family name: ");
+        scanf("%s",&str);
 
-    
+    system("clear");
 
 }
