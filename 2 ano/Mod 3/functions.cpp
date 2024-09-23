d382771e47637067a56cda1306828b38cd8c28ed
mvvalerio <martimvv07@gmail.com>
2024-09-23 10:08:11 +0000

    Updated

---
+++ 2 ano/Mod 3/functions.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*int soma(int lista[]){
+
+    int soma = 0;
+
+    for(int x = 0; x < 5; x++){
+
+        soma += lista[x];
+    }
+
+    return soma;
+}
+*/
+
+/*float soma(float x, float y = 10.2){
+
+    return x+y;
+}
+*/
+
+ 
+
+
+int main(){
+
+
+    return 0;
+}
